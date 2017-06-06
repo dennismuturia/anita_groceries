2661ef5620acffa9ff518b27231a1082f0d8779d
dennis292 <dennis292@localhost.localdomain>
2017-06-06 09:47:09 +0300

    A littl modification

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
+$(document).ready(function() {
+  $("form#groceries").submit(function(event) {
+    var gross = ['veg1', 'veg2', 'veg3', 'veg4'];
+    var array_items = []
 
-$(document).ready(function(){
-  $("form#groceries").submit(function(event){
-      var gross = ['veg1','veg2','veg3','veg4'];
-
-      gross.forEach(function(gros){
-        var customer = $('input#'+ gros).val()
-        $('.'+gros).text(customer);
-      })
-      event.preventDefault();
+    gross.forEach(function(gros) {
+      var customer = $('input#' + gros).val().toUpperCase()
+      $(array_items.push(gros))
+      alert(array_items)
+    //  $('.' + gros).text(customer);
+    })
+    event.preventDefault();
   })
 })
